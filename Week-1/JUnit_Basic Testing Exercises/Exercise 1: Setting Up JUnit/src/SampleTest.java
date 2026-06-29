478f23d8987bf69777bf24a85ecfdcb6344d3597
thithikshamv <thithikshamv@gmail.com>
2026-06-29 23:25:24 +0530

    Add files via upload

---
+++ Week-1/JUnit_Basic Testing Exercises/Exercise 1: Setting Up JUnit/src/SampleTest.java
@@ -0,0 +1,9 @@
+package com.cts;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class SampleTest {
+    @Test
+    public void testAddition(){
+        assertEquals(5,2 + 3);
+    }
+}
