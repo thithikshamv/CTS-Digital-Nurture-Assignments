d474aae453c2bc5989310bc3af072e5909811194
thithikshamv <thithikshamv@gmail.com>
2026-06-29 23:18:34 +0530

    Add files via upload

---
+++ Week-1/Design Patterns and Principles /Exercise 2: The Factory Method Pattern/SourceCode/ExcelFactory.java
@@ -0,0 +1,10 @@
+public class ExcelFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+
+        return new ExcelDocument();
+
+    }
+
+}
---
+++ Week-1/Design Patterns and Principles /Exercise 2: The Factory Method Pattern/SourceCode/PdfFactory.java
@@ -0,0 +1,10 @@
+public class PdfFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+
+        return new PdfDocument();
+
+    }
+
+}
