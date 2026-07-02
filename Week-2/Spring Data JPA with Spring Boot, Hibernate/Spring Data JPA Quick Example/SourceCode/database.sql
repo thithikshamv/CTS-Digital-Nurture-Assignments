df5dddaec1579815d5689ad6d90491dacd3b203d
thithikshamv <thithikshamv@gmail.com>
2026-07-02 15:29:01 +0530

    Create database.sql

---
+++ Week-2/Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA Quick Example/SourceCode/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE ormlearn;
+
+USE ormlearn;
+
+CREATE TABLE country (
+    code VARCHAR(2) PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+INSERT INTO country VALUES ('IN', 'India');
+INSERT INTO country VALUES ('US', 'United States of America');
+
+SHOW TABLES;
+
+SELECT * FROM country;
