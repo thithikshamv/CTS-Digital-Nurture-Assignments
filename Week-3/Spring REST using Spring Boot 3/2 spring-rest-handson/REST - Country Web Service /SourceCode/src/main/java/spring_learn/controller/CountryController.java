c33b0069bf2652410dc81b008dd9cee93cf2b69d
thithikshamv <thithikshamv@gmail.com>
2026-07-06 01:00:10 +0530

    Create CountryController.java

---
+++ Week-3/Spring REST using Spring Boot 3/2 spring-rest-handson/REST - Country Web Service /SourceCode/src/main/java/spring_learn/controller/CountryController.java
@@ -0,0 +1,23 @@
+package spring_learn.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring_learn.Country;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        return country;
+    }
+}
