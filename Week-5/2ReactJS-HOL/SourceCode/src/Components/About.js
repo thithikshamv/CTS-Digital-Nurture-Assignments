d845696a5496053924f6b4a3a1b27fd922aa7a84
thithikshamv <thithikshamv@gmail.com>
2026-07-12 16:48:42 +0530

    Create About.js

---
+++ Week-5/2ReactJS-HOL/SourceCode/src/Components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <h1>Welcome to the About page of the Student Management Portal</h1>
+    );
+  }
+}
+
+export default About;
