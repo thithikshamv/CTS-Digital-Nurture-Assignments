286739235b3feebbac1a236eea388d91bba64fe9
thithikshamv <thithikshamv@gmail.com>
2026-07-31 01:27:15 +0530

    Add files via upload

---
+++ Week-6/ANGULAR/student-course-portal/src/app/services/course.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  private apiUrl = 'http://localhost:3000/courses';
+
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.apiUrl);
+  }
+
+  getCourseById(id: number): Observable<Course> {
+    return this.http.get<Course>(`${this.apiUrl}/${id}`);
+  }
+
+  createCourse(course: Omit<Course, 'id'>): Observable<Course> {
+    return this.http.post<Course>(this.apiUrl, course);
+  }
+
+  updateCourse(course: Course): Observable<Course> {
+    return this.http.put<Course>(`${this.apiUrl}/${course.id}`, course);
+  }
+
+  deleteCourse(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+}
