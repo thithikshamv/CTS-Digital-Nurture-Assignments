f14570826012a1a8153378f0d960b53e1c2bb9eb
thithikshamv <thithikshamv@gmail.com>
2026-07-31 01:36:57 +0530

    Add files via upload

---
+++ Week-6/ANGULAR/student-course-portal/src/app/pages/student-profile/student-profile.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EnrollmentService } from '../../services/enrollment';
+import { Course } from '../../models/course.model';
+
+@Component({
+  selector: 'app-student-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student-profile.html',
+  styleUrl: './student-profile.css'
+})
+export class StudentProfile implements OnInit {
+
+  enrolledCourses: Course[] = [];
+
+  constructor(private enrollmentService: EnrollmentService) {}
+
+  ngOnInit(): void {
+
+    this.enrollmentService.getEnrolledCourses().subscribe({
+
+      next: (courses) => {
+        this.enrolledCourses = courses;
+      },
+
+      error: (err) => {
+        console.error(err);
+      }
+
+    });
+
+  }
+
+}
